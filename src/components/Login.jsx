b88c398e241bd8cbd7f77aa425718b71c59e8847
fed1k <allamurotovcoder@mail.ru>
2023-03-01 21:47:50 +0500

    auth validation fixes and add password hide and show functionality

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,9 +4,11 @@ import { useRef } from "react";
 import { auth, provider } from "../firebase/firebaseConfig";
 import { FcGoogle } from "react-icons/fc"
 import { useState } from "react";
+import {BiShow, BiHide} from "react-icons/bi"
 const Login = () => {
 
   const [error, setError] = useState()
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false)
 
   let emailRef = useRef()
   let passwordRef = useRef()
@@ -19,7 +21,11 @@ const Login = () => {
       const user = await signInWithEmailAndPassword(auth, emailRef.current.value, passwordRef.current.value)
       navigate("/home");
     } catch(error) {
-      if(error.message === "Firebase: Error (auth/user-not-found).") setError("User does not exist");
+      if(error.message === "Firebase: Error (auth/user-not-found)."){
+        setError("User does not exist");
+      }else if(error.code === "auth/wrong-password") {
+        setError("Wrong password")
+      }
     }
   }
 
@@ -29,14 +35,19 @@ const Login = () => {
     }).catch(error => console.log(error))
   }
 
+  const passwordVisibilityToggle = () => {
+    setIsPasswordVisible(prev => prev ? false : true);
+  }
+
   return (
     <div className="auth-page">
       <h2>Login</h2>
       <form onSubmit={handleLogin}>
         <input type="email" placeholder="johndoe@gmail.com" ref={emailRef} />
-        <input type="password" placeholder="Password" ref={passwordRef}/>
-        {error ? <span style={{color: "#FF7C7CFF"}} >{error}</span> : null}
+        <input type={`${isPasswordVisible ? "text" : "password"}`} placeholder="Password" ref={passwordRef}/>
+        {isPasswordVisible ? <BiHide className="show-password" onClick={passwordVisibilityToggle} /> : <BiShow className="show-password" onClick={passwordVisibilityToggle}/>}
         <button className="login-btn" type="submit">Login</button>
+        {error ? <span style={{color: "#FF7C7CFF"}} >{error}</span> : null}
         <button id="google-login-btn" type="button" onClick={handleGoogleLogin}><FcGoogle /> Sign in with Google</button>
         <Link to="/signup" className="signup-link">Signup</Link>
       </form>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -4,11 +4,13 @@ import { createUserWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import { auth, provider } from "../firebase/firebaseConfig"
 import { FcGoogle } from "react-icons/fc"
 import { useState } from "react";
+import { BiHide, BiShow } from "react-icons/bi";
 const Signup = () => {
 
   let navigate = useNavigate()
 
   const [error, setError] = useState()
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false)
 
   let emailRef = useRef();
   let passwordRef = useRef();
@@ -19,7 +21,9 @@ const Signup = () => {
       const user = await createUserWithEmailAndPassword(auth, emailRef.current.value, passwordRef.current.value)
       navigate("/home");
     } catch(error) {
-      if(error.code){
+      if(error.code === "auth/weak-password"){
+        setError("Password should be at least 6 characters")
+      } else if(error.code === "auth/email-already-in-use") {
         setError("Email already in use")
       }
     }
@@ -31,12 +35,17 @@ const Signup = () => {
     }).catch(error => console.log(error))
   }
 
+  const passwordVisibilityToggle = () => {
+    setIsPasswordVisible(prev => prev ? false : true);
+  }
+
   return (
     <div className="auth-page">
       <h2>Signup</h2>
       <form onSubmit={handleSignup}>
         <input type="email" placeholder="johndoe@gmail.com" ref={emailRef} />
-        <input type="password" placeholder="Password" ref={passwordRef}/>
+        <input type={`${isPasswordVisible ? "text" : "password"}`} placeholder="Password" ref={passwordRef}/>
+        {isPasswordVisible ? <BiHide className="show-password" onClick={passwordVisibilityToggle} /> : <BiShow className="show-password" onClick={passwordVisibilityToggle}/>}
         {error ? <span style={{color: "#FF7C7CFF"}}>{error}</span> : null}
         <button className="signup-btn" type="submit">Signup</button>
         <button id="google-signup-btn" type="button" onClick={handleGoogleSignup}><FcGoogle /> Sign up with Google</button>
